a7fdbf1e27065788f7dac539c7631416644f8998
akshaya <akshayakrishnan318@gmail.com>
2023-01-22 00:17:08 +0530

    ch 9 revised

--- Chapter9/madlibs.py
+++ Chapter9/madlibs.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 import re
+import shutil
 
-
+shutil.copy(Path.home()/'ch9.txt',Path.home()/'ch9replaced.txt')
 l=[]
 ch9file=open(Path.home() / 'ch9.txt')
 ch9filecontent=ch9file.read()
@@ -13,4 +14,8 @@ for i in l:
     word=input(f'enter a {i}')
     ch9filecontent=ch9filecontent.replace(i,word,1) #replace the match with the inputed value
 #that 1 is to replace it once otherwise it will replace all instances
+new=open('ch9replaced.txt','w')
+new.write(ch9filecontent)
 print(ch9filecontent)
+new.close()
+#the new file will be created in the current directory
